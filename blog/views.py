f1d1822d30983fe591e5b81dc1cc69a5607e9baa
thapelomagqazana <tapsmcgzee8@gmail.com>
2024-02-08 17:51:45 +0200

    Added functionality for retrieving posts by similarity

--- blog/views.py
+++ blog/views.py
@@ -6,6 +6,7 @@ from .models import Post, Comment
 from .forms import EmailPostForm, CommentForm
 from django.views.decorators.http import require_POST
 from taggit.models import Tag
+from django.db.models import Count
 
 
 def post_list(request, tag_slug=None):
@@ -48,11 +49,19 @@ def post_detail(request, year, month, day, post):
     # Form for users to comment
     form = CommentForm()
 
+    # List of similar posts
+    post_tags_ids = post.tags.values_list("id", flat=True)
+    similar_posts = Post.published.filter(tags__in=post_tags_ids)\
+                                  .exclude(id=post.id)
+    similar_posts = similar_posts.annotate(same_tags=Count("tags"))\
+                                  .order_by("-same_tags", "-publish")[:4]
+
     return render(request,
                   "blog/post/detail.html",
                   {"post": post,
                    "comments": comments,
-                   "form": form})
+                   "form": form,
+                   "similar_posts": similar_posts})
 
 
 def post_share(request, post_id):
